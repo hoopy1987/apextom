2404cad3572a2a4091636446c732ef5ced03681e
hoopy1987 <luyushengo@gmail.com>
2013-10-25 10:37:46 +0800

    add

---
+++ src/main/java/com/apextom/exception/UnMacthingDataException.java
@@ -0,0 +1,15 @@
+package com.apextom.exception;
+
+/**
+ * @author Administrator
+ *
+ */
+public class UnMacthingDataException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public UnMacthingDataException(String message) {
+		super(message);
+	}
+
+}
