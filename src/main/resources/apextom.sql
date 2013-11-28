d12ea679ab538f8bfa2c0e7f7ca292e0aaa6d0b9
hoopy1987 <luyushengo@gmail.com>
2013-11-28 18:21:13 +0800

    sql

---
+++ src/main/resources/apextom.sql
@@ -0,0 +1,85 @@
+﻿# Host: 192.168.1.114  (Version: 5.5.17)
+# Date: 2013-11-28 18:18:46
+# Generator: MySQL-Front 5.3  (Build 2.42)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+DROP DATABASE IF EXISTS `apextom`;
+CREATE DATABASE `apextom` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `apextom`;
+
+#
+# Source for table "projects"
+#
+
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE `projects` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(255) NOT NULL DEFAULT '' COMMENT '栏目名称',
+  `ptype` char(1) NOT NULL DEFAULT '' COMMENT '栏目类型（0：常规栏目，1：课程类，2：下载类）',
+  `orders` int(3) DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='栏目表';
+
+#
+# Data for table "projects"
+#
+
+INSERT INTO `projects` VALUES (1,'最新动态','0',0),(2,'资料下载','2',1),(3,'TOEFL','1',0),(4,'SAT','1',1);
+
+#
+# Source for table "sys_parms"
+#
+
+DROP TABLE IF EXISTS `sys_parms`;
+CREATE TABLE `sys_parms` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `param_key` varchar(255) NOT NULL DEFAULT '' COMMENT '参数名',
+  `param_value` varchar(255) NOT NULL DEFAULT '' COMMENT '参数值',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统参数表';
+
+#
+# Data for table "sys_parms"
+#
+
+
+#
+# Source for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `loginuser` varchar(50) NOT NULL DEFAULT '' COMMENT '登录名',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `username` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名称',
+  `rights` char(1) NOT NULL DEFAULT '0' COMMENT '权限（0：管理员，1：课程设置）',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`Id`,`loginuser`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+#
+# Data for table "users"
+#
+
+INSERT INTO `users` VALUES (1,'apextom','4QrcOUm6Wau+VuBX8g+IPg==','系统管理员','0','系统权限');
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
